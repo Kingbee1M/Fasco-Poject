f2cc10621a7ef02e7b29dd78e815481b91b24fd4
Kingbee1M <160544114+Kingbee1M@users.noreply.github.com>
2025-09-05 22:18:14 +0100

    finished the Landing page, Navgigation and Fotter

--- src/UI/Footer.tsx
+++ src/UI/Footer.tsx
@@ -177,6 +177,23 @@ export default function Footer() {
             </div>
           <Image src={"/images/news2.svg"} alt="newsletter" width={150} height={150} className="w-auto hidden lg:flex" />
         </div>
+
+
+        <div className="w-full h-auto py-10 border-t-2 border-[var(--light-gray2)] flex flex-col justify-center items-start gap-5 px-3  lg:items-center lg:gap-20">
+          <div  className="w-full flex flex-col justify-center items-start gap-5 lg:flex-row lg:justify-between lg:items-center lg:w-9/10">
+            <p className="text-[var(--grey-text2)] text-[17px] lg:text-[25px] font-extrabold">FASCO</p>
+            <ul className="flex flex-col justify-center items-start gap-5 ml-0 lg:flex-row lg:justify-end">
+              <li className="text-[var(--grey-text2)] text-[12px] lg:text-[16px]">Support Center</li>
+              <li className="text-[var(--grey-text2)] text-[12px] lg:text-[16px]">Invoicing</li>
+              <li className="text-[var(--grey-text2)] text-[12px] lg:text-[16px]">Contract</li>
+              <li className="text-[var(--grey-text2)] text-[12px] lg:text-[16px]">Careers</li>
+              <li className="text-[var(--grey-text2)] text-[12px] lg:text-[16px]">Blog</li>
+              <li className="text-[var(--grey-text2)] text-[12px] lg:text-[16px]">FAQs</li>
+            </ul>
+          </div>
+          <p className="w-full text-[var(--grey-text2)] text-[10px] text-center lg:text-[16px]">copyright &copy; 2024 Fasco. All rights reserved.</p>
+        </div>
+
       </div>
   )
 }
